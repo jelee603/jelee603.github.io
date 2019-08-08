3e24d5c54ecdcdc4c2cc8451678f07d58d99cda6
jieun <jelee@ex-em.com>
2019-08-08 18:22:23 +0900

    code 작성

--- code/2019-08-07.js
+++ code/2019-08-07.js
@@ -2,8 +2,11 @@
 function countSwaps(a) {
     let count = 0
     let arr = []
-    for (let idx = 0, len = a.length; idx < len; idx++) {
-        if (a[idx] > a[idx + 1]) {
+    for (let idx = a.length, len = 0; len < idx; idx--) {
+        // 마지막에서 한칸씩 앞으로 오면서 바로 이전 값과 비교 => n, n-1
+        // 큰 값이면 뒤로 이동
+        // 이와 같은 반복을 정렬이 끝날때까지 계속 한다.
+        if (a[idx] < a[idx - 1]) {
             const [v1, v2] = swap(a[idx], a[idx + 1])
             arr.push(v1)
             arr.push(v2)
