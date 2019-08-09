0a6ac1f011985b20682c398a1a198630a55311ee
jieun <jelee@ex-em.com>
2019-08-09 13:26:57 +0900

    gitingore 적용

--- code/2019-08-07.js
+++ code/2019-08-07.js
@@ -1,13 +1,16 @@
-// [Solved] Sorting: Bubble Sort
+/**
+ * Sorting: Bubble Sort
+ *
+ * 마지막에서 한칸씩 앞으로 오면서 바로 이전 값과 비교 => n, n-1
+ * 큰 값이면 뒤로 이동
+ * 이와 같은 반복을 정렬이 끝날때까지 계속 한다. *
+ */
+
 function countSwaps(n, a) {
     let count = 0
     // const min = Math.min.apply(null, a)
-
     for (let loop = 0; loop < n; loop++) {
         for (let idx = a.length, len = 0; len < idx; idx--) {
-            // 마지막에서 한칸씩 앞으로 오면서 바로 이전 값과 비교 => n, n-1
-            // 큰 값이면 뒤로 이동
-            // 이와 같은 반복을 정렬이 끝날때까지 계속 한다.
 
             let [prev, curr] = [a[idx-1], a[idx]]
             if (prev > curr) {
@@ -23,7 +26,6 @@ function countSwaps(n, a) {
     console.log(`First Element: ${a[0]}`)
     console.log(`Last Element: ${a[a.length-1]}`)
 }
-
 
 // countSwaps([1, 2, 3])
 // countSwaps([2, 1, 5, 3, 4])
