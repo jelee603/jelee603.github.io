51ff677aa6e9cb158d4cba4d7eef3feebc78f220
jieun <jelee@ex-em.com>
2019-08-12 13:39:00 +0900

    swap code

---
+++ code/2019-08-12.js
@@ -0,0 +1,31 @@
+/**
+ * Minimum Swaps 2
+ *
+ * swap 해서 오름차순으로 정렬하는데 최소 swap 횟수를 센다. *
+ */
+function minimumSwaps(arr) {
+    const start = 0;
+    let count = 0;
+    while (0 < arr.length) {
+        const min = Math.min.apply(null, arr)
+        const min_idx = arr.indexOf(min)
+
+        if (start === min_idx) {
+            arr.splice(min_idx, 1)
+        } else {
+            const [prev, curr] = [arr[min_idx], arr[start]]
+
+            arr[start] = prev
+            arr[min_idx] = curr
+            arr.splice(start, 1)
+            count++
+        }
+    }
+
+    console.log(count)
+    return count;
+}
+
+
+minimumSwaps([1, 3, 5, 2, 4, 6, 7])
+minimumSwaps([4, 3, 1, 2])
