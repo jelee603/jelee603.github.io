ce9c73cfa9594f88f42e0fc42a8efe3b9c0968e6
jieun <jelee@ex-em.com>
2019-08-14 13:24:06 +0900

    code readme 추가

--- code/2019-08-14.js
+++ code/2019-08-14.js
@@ -1,13 +1,15 @@
 /**
  * Sherlock and Anagrams
- * 
- * 문자열이 주어지고 애니그램은 하나의 문자와 다른 문자의 재배열이 쌍을 이루는 것을 말한다.
- * 각각 다른 애니그램의 갯수를 출력한다.
+ *
+ * 애너그램은 하나의 문자를 다른 문자로 재배열해서 다른 뜻을 가진 문자로 바꾸는 것을 말한다.
+ * 문자열이 주어지고 문자열 중에 단어를 재배열하면 다른 문자가 된다.
+ * 각 다른 애너그램의 단어의 pair 갯수를 출력한다.
  */
 
 function sherlockAndAnagrams(s) {
   let sameArr = []
   let count = 0
+
   Array.from(s).forEach(v => {
     if(!sameArr.includes(v)) { 
       sameArr.push(v)
@@ -15,11 +17,9 @@ function sherlockAndAnagrams(s) {
     // console.log(v)
   })
 
-  count = sameArr.length + 1; 
-  
-  if (sameArr.length === s.length) {
-    count = 0
-  } else if (sameArr.length %2 === 0){
+  if (sameArr.length !== s.length) {
+    count = sameArr.length + 1;
+  } else if (sameArr.length % 2 === 0){
     count = sameArr.length + 2;
   }
   console.log(count)
@@ -28,6 +28,6 @@ function sherlockAndAnagrams(s) {
   // console.log(sameArr)
 }
 
-sherlockAndAnagrams("cdcd")
+sherlockAndAnagrams("cdcd") // [c, c], [d, d], [cd, cd], [cd, dc], [dc, cd]
 sherlockAndAnagrams("abba")
 sherlockAndAnagrams("abcd")
---
+++ code/readme.md
@@ -0,0 +1,16 @@
+### 일일 알고리즘
+
+#### Pattern
+- 2019-06-29 : Observer 패턴
+
+#### Arrays
+- 2019-08-02 : Left Rotation
+- 2019-08-12 : Minimum Swaps 2 (확인필요)
+
+#### Sorting
+- 2019-08-07 : Sorting: Bubble Sort
+- 2019-08-13 : Mark and Toys
+
+#### Dictionaries
+- 2019-08-11 : Two Strings
+- 2019-08-14 : Sherlock and Anagrams (확인필요)
